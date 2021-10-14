b6fdee32143198bf0f336884ab11b2e244bb764a
Santosh Bhandari <contact@santoshb.com.np>
2021-10-14 12:56:02 +0545

    fix: Add ability to decompress latest files
    Refer to https://github.com/Galaxy1036/Dumpsc/commit/db8a7d8956a348f6716eaf381e03571253228e43 for changes

--- System/Dumpsc.py
+++ System/Dumpsc.py
@@ -11,6 +11,7 @@ from PIL import Image
 
 from System.Logger import Console
 
+
 def convert_pixel(pixel, type):
     if type in (0, 1):
         # RGB8888
@@ -38,56 +39,61 @@ def convert_pixel(pixel, type):
     else:
         return None
 
-def decompress_data(data, baseName = "Unknown"):
-        version = None
 
-        if data[:2] == b'SC':
-            # Skip the header if there's any
-            version = int.from_bytes(data[2: 6], 'big')
+def decompress_data(data, baseName="Unknown"):
+    version = None
+
+    if data[:2] == b'SC':
+        # Skip the header if there's any
+        pre_version = int.from_bytes(data[2: 6], 'big')
+
+        if pre_version == 4:
+            version = int.from_bytes(data[6: 10], 'big')
+            hash_length = int.from_bytes(data[10: 14], 'big')
+            end_block_size = int.from_bytes(data[-4:], 'big')
+
+            data = data[14 + hash_length:-end_block_size - 9]
+
+        else:
+            version = pre_version
             hash_length = int.from_bytes(data[6: 10], 'big')
             data = data[10 + hash_length:]
 
-        if version in (None, 1, 3):  # Version 2 in HDP does not use any compression
+    if version in (None, 1, 3):
+        try:
             if data[:4] == b'SCLZ':
                 Console.info(' ├── Detected LZHAM compression')
 
                 dict_size = int.from_bytes(data[4:5], 'big')
                 uncompressed_size = int.from_bytes(data[5:9], 'little')
-
-                try:
-                    decompressed = lzham.decompress(data[9:], uncompressed_size, {'dict_size_log2': dict_size})
-
-                except Exception as exception:
-                    Console.error('Cannot decompress {} !'.format(baseName))
-                    return
+                decompressed = lzham.decompress(data[9:], uncompressed_size, {
+                                                'dict_size_log2': dict_size})
 
             elif data[:4] == bytes.fromhex('28 B5 2F FD'):
                 Console.info(' ├── Detected Zstandard compression')
-
-                try:
-                    decompressed = zstandard.decompress(data)
-
-                except Exception as exception:
-                    Console.error('Cannot decompress {} !'.format(baseName))
-                    return
+                decompressed = zstandard.decompress(data)
 
             else:
                 Console.info(' ├── Detected LZMA compression')
                 data = data[0:9] + (b'\x00' * 4) + data[9:]
+                decompressor = lzma.LZMADecompressor()
 
-                try:
-                    decompressed = lzma.LZMADecompressor().decompress(data)
+                output = []
 
-                except Exception as exception:
-                    Console.error('Cannot decompress {} !'.format(baseName))
-                    return
+                while decompressor.needs_input:
+                    output.append(decompressor.decompress(data))
 
-            Console.info(' └── Successfully decompressed {} '.format(baseName))
+                decompressed = b''.join(output)
 
-        else:
-            decompressed = data
-        
-        return decompressed
+        except Exception as _:
+            Console.info(' └── Cannot decompress {} !'.format(baseName))
+            return
+
+    else:
+        decompressed = data
+
+    return decompressed
+
 
 def process_sc(baseName, data, path, decompress):
     if decompress:
@@ -125,7 +131,8 @@ def process_sc(baseName, data, path, decompress):
 
         for y in range(height):
             for x in range(width):
-                pixels.append(convert_pixel(decompressed[i:i + pixelSize], subType))
+                pixels.append(convert_pixel(
+                    decompressed[i:i + pixelSize], subType))
                 i += pixelSize
 
         img.putdata(pixels)
@@ -144,23 +151,25 @@ def process_sc(baseName, data, path, decompress):
                 # line end blocks
                 for j in range(32):
                     for h in range(width % 32):
-                        imgl[h + (width - (width % 32)), j + (l * 32)] = pixels[iSrcPix]
+                        imgl[h + (width - (width % 32)), j +
+                             (l * 32)] = pixels[iSrcPix]
                         iSrcPix += 1
             # final lines
             for k in range(width // 32):  # 32-pixels blocks in a line
                 for j in range(height % 32):  # line in a multi line block
                     for h in range(32):  # pixels in a 32-pixels-block
-                        imgl[h + (k * 32), j + (height - (height % 32))] = pixels[iSrcPix]
+                        imgl[h + (k * 32), j + (height - (height % 32))
+                             ] = pixels[iSrcPix]
                         iSrcPix += 1
             # line end blocks
             for j in range(height % 32):
                 for h in range(width % 32):
-                    imgl[h + (width - (width % 32)), j + (height - (height % 32))] = pixels[iSrcPix]
+                    imgl[h + (width - (width % 32)), j +
+                         (height - (height % 32))] = pixels[iSrcPix]
                     iSrcPix += 1
 
         img.save(path + baseName + ('_' * picCount) + '.png', 'PNG')
         images.append(img)
         picCount += 1
-    
-    return images
 
+    return images
